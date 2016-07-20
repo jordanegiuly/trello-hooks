fb41627e312ee169751decc4e0c38ecbc6b6c887
Jordane Giuly <jordane@spendesk.com>
2016-07-20 14:32:22 +0200

    start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,6 @@
 cd /var/www/app/
+echo which node
+echo node --version
+echo npm --version
 forever stopall
-forever start app/index.js
+forever start -c /home/ubuntu/.nvm/v5.11.0/bin/node app/index.js
