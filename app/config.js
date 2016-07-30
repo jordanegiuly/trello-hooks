8ddfebf8a54f5dc36be052b64da83ebe64e4779a
Jordane Giuly <jordane@spendesk.com>
2016-07-30 12:33:55 +0200

    handle new labels

--- app/config.js
+++ app/config.js
@@ -12,14 +12,47 @@ const config = {
 		boards: [
 			'Spendesk - Feedback'
 		],
-		auth: auth		
+		auth: auth,
+		hooks: [
+			{
+				trigger: {
+					actionType: 'createCard',
+					modelName: 'Spendesk - Feedback'
+				},
+				action: 'addMemberAndLabel',
+				config: {
+					labels: {
+						'Customer feature requests': ['Customer feature', '# user story #'],
+						'Team feature requests': ['Team feature', '# user story #'],
+						'Improvements': ['Improvment', '# task #'],
+						'Bugs': ['Bug', '# task #'],
+						'Wording / UI issues': ['Wording / UI', '# task #']
+					}					
+				}
+			}
+		]	
 	},
 	development: {
 		appName: 'trelloHooks',
-		callbackURL: 'http://745f858c.ngrok.io/trellohooks',
+		callbackURL: 'http://660f9b68.ngrok.io/trellohooks',
 		boards: [
 			'Personal tasks'
 		],
-		auth: auth
+		auth: auth,
+		hooks: [
+			{
+				trigger: {
+					actionType: 'createCard',
+					modelName: 'Personal tasks'
+				},
+				action: 'addMemberAndLabel',
+				config: {
+					labels: {
+						'TODO TODAY': ['Assigned'],
+						'DOING': ['In progress', 'Important']
+					}					
+				}
+			}
+		]
 	}
 }
